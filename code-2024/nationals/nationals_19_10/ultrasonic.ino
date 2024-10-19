70c5baf85bcf986f378424f418387f319c912eb4
General-Hex <88964979+General-Hex@users.noreply.github.com>
2024-10-19 19:23:22 +0800

    19_10 update

---
+++ code-2024/nationals/nationals_19_10/ultrasonic.ino
@@ -0,0 +1,132 @@
+class Ultrasonic {
+public:
+  int trigger_pin;
+  int echo_pin;
+
+  Ultrasonic(int TRIGGER_PIN, int ECHO_PIN) {
+    trigger_pin = TRIGGER_PIN;
+    echo_pin = ECHO_PIN;
+  }
+
+  int getDistance() {
+    long duration, ult_distance;
+
+    // clear trigger pin
+    digitalWrite(trigger_pin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger_pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger_pin, LOW);
+
+    // get duration in miliseconds
+    duration = pulseIn(echo_pin, HIGH, 20000);
+    
+    // converet to cm
+    ult_distance = (duration / 29) / 2;  // Calculating the ult_distance
+
+    return ult_distance;
+  }
+};
+
+class PositionSystem {
+private:
+  Ultrasonic ultLeft = Ultrasonic(USL_TRIG, USL_ECHO);
+  Ultrasonic ultRight = Ultrasonic(USR_TRIG, USR_ECHO);
+  Ultrasonic ultBack = Ultrasonic(USB_TRIG, USB_ECHO);
+
+  // LEFT, RIGHT, BACK
+  int rawDistances[3] = {0};
+  float trueDistances[3] = {0};
+  float previousDistances[3] = {0};
+
+  float deltaDistances[3] = {0};
+
+  void getRawDistances() {
+    rawDistances[0] = ultLeft.getDistance() + ULTRASONIC_TO_ROBOT;
+    rawDistances[1] = ultRight.getDistance() + ULTRASONIC_TO_ROBOT;
+    rawDistances[2] = ultBack.getDistance() + ULTRASONIC_TO_ROBOT;
+  };
+
+  void getTrueDistances(float tilt) { // tilt in radians
+    for (int i = 0; i < 3; i++) {
+      trueDistances[i] = cos(tilt)*rawDistances[i];
+    }
+  };
+
+  bool totalWidthCompare() {
+    return abs((trueDistances[0]+trueDistances[1])-FIELD_WIDTH) < 20;
+  };
+
+  void getDeltaDistances() {
+    for (int i = 0; i < 3; i++) {
+      deltaDistances[i] = abs(trueDistances[i]-previousDistances[i]);
+    }
+  };
+
+  void logPreviousDistances() {
+    for (int i = 0; i < 3; i++) {
+      previousDistances[i] = trueDistances[i];
+    }
+  };
+
+public:
+  bool reliable = true;
+  int usingIndex = 0; 
+
+  float x, y;
+  void chooseULT() {
+    getDeltaDistances();
+
+    if (deltaDistances[0] > 30 && deltaDistances[1] > 30) {
+      usingIndex = 0;
+      reliable = false;
+    }
+    else if (deltaDistances[0] > 30) {
+      usingIndex = 1;
+      reliable = true;
+    }
+    else if (deltaDistances[1] > 30) {
+      usingIndex = 0;
+      reliable = true;
+    }
+    else {
+      usingIndex = 0;
+      reliable = true;
+    }
+  };
+
+  void update(float tilt) {
+    getRawDistances();
+    getTrueDistances(tilt);
+
+    if (!totalWidthCompare()) {
+      Serial.println("Choose ULT");
+      chooseULT();
+    }
+    
+    if (usingIndex == 0) {
+      x = trueDistances[0];
+    }
+    else if (usingIndex == 1) {
+      x = FIELD_WIDTH - trueDistances[1];
+    }
+
+    y = FIELD_LENGTH - trueDistances[2];
+
+    if (totalWidthCompare()) {
+      logPreviousDistances();
+    }
+  };
+
+  void serialPrint() {
+    Serial.print("TrueLeft: ");
+    Serial.print(trueDistances[0]);
+    Serial.print(" TrueRight: ");
+    Serial.println(trueDistances[1]);
+
+    Serial.print("Reliable: ");
+    Serial.print(reliable);
+    Serial.print(" usingIndex: ");
+    Serial.println(usingIndex);
+  }
+};
---
+++ code-2024/nationals/nationals_19_10/y_robot.ino
@@ -0,0 +1,182 @@
+class Robot {
+public:
+  // defining obkects
+  MotorController mc = MotorController(false);
+  Adafruit_BNO055 bno = Adafruit_BNO055(55);
+  IRSensor ir;
+
+  PositionSystem ps;
+
+  // defining variables
+  float tilt = 0;
+  float offset = 0;
+  int direction = 0, strength = 0;
+
+  float x = 0, y = 0;
+
+  int lineValue;
+
+private:
+  /* :::::::: FUNCTIONS :::::::: */
+  void getSensorData() {
+    // READ IR
+    ir.readData();
+    direction = ir.getFilteredDirection();
+    strength = ir.getFilteredStrength();
+
+    // READ IMU
+    sensors_event_t event;
+    bno.getEvent(&event);
+    tilt = event.orientation.x;
+
+    // convert to unit circle tilt
+    tilt = 360 - tilt;
+
+    // get values from line sensor
+    if (Serial6.available()) {
+      lineValue = Serial6.read() - 1;
+    }
+  };
+  
+  void getBallXY(float& ballX, float& ballY) {
+    ballX = cos(irAngles[direction]);
+    ballY = sin(irAngles[direction]);
+  };
+
+public:
+  void update() {
+    // get sensor data using sensor classes
+    getSensorData();
+    Serial.println("Read sensors"); // code crashes without line between for some reason
+    ps.update(tilt*PI/180);
+  };
+
+  void getBehindBall() {
+    if (direction == 0) {
+      x = 0, y = 0;
+      return;
+    }
+
+    if (strength < 36) {
+      getBallXY(x, y);
+      return;
+    }
+
+    if (direction == 12) {
+      x = 0, y = 1;
+      return;
+    }
+
+    if (direction == 6) {
+      if (ps.x < FIELD_WIDTH/2 + 10) {
+        x = 1, y = 0;
+      }
+      else {
+        x = -1, y=0;
+      }
+      
+      return;
+    }
+
+    float movementAngle;
+
+    float ballX = 0, ballY = 0;
+    getBallXY(ballX, ballY);
+
+    if (ballX < 0) {
+      movementAngle = irAngles[direction] + PI/2;
+    }
+    else {
+      movementAngle = irAngles[direction] - PI/2;
+    }
+
+    x = cos(movementAngle);
+    y = sin(movementAngle);
+  };
+
+  void adjustSpeed() {
+    if (lineValue != 0) {
+      mc.setSpeed(LINE_SPEED);
+      return;
+    }
+
+    if (irAngles[direction] > PI && irAngles[direction] < 2*PI) {
+      mc.setSpeed(BALL_BEHIND_SPEED);
+    }
+    else {
+      mc.setSpeed(100);
+    }
+  };
+
+  void getOffset() {
+    if (lineValue != 0) { // UPDATE if ultrasonics not reliable do not tilt (risky needs testing)
+      offset = 0;
+      return;
+    }
+
+    if (ps.x < 50) {
+      offset = -16;
+    }
+    else if (FIELD_WIDTH - ps.x < 50) {
+      offset = 16;
+    }
+    else if (ps.x < FIELD_WIDTH/2 - 20) {
+      offset = -10;
+    }
+    else if (ps.x > FIELD_WIDTH/2 + 20) {
+      offset = 10;
+    }
+    else {
+      offset = 0;
+    }
+  };
+
+  // gets the goal x,y coordinants using ultrasonics
+  void getGoalXY(float &x, float &y) {
+    x = -(ps.x - GOAL_POS_X);
+    y = ps.y - GOAL_POS_Y;
+  };
+
+  void targetGoal() {
+    x = 0, y = 1;
+    // if (!ps.reliable || direction != 12) {
+    //   return;
+    // }
+    // getGoalXY(x, y);
+  };
+
+  // stays within the lines
+  void stopAtLine() {
+    if (lineValue == 1) { // && y > 0
+      x = 0, y = -1;
+    }
+    else if (lineValue == 4) { // && y < 0
+      x = 0, y = 1;
+    }
+    else if (lineValue == 2) { // && (x < 0)
+      if (ps.x < 40) {
+        x = 1, y = 0;
+      }
+      else {
+        // set speed to 100 as last resort to get out quick
+        mc.setSpeed(100);
+        x = -1, y = 0;
+      }
+    }
+    else if (lineValue == 3) { //&& (x > 0)
+      if (ps.x < 40) {
+        // set speed to 100 as last resort to get out quick
+        x = 1, y = 0;
+      }
+      else {
+        mc.setSpeed(100);
+        x = -1, y = 0;
+      }
+    }
+  };
+
+  void run() {
+    // run motors using motor controller
+    mc.runMotors(x, y, tilt, offset);
+  };
+};
