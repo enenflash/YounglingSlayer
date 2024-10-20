bac235a078958210ab8f005c1d3e61901e6d2380
General-Hex <88964979+General-Hex@users.noreply.github.com>
2024-10-20 21:43:06 +1000

    added final nationals

---
+++ code-2024/nationals/Final-Nationals-20-10/ir.ino
@@ -0,0 +1,88 @@
+// ir sensor class
+class IRSensor {
+private:
+  // variable initilisations
+  int strength;
+  int direction;
+  int dirHistory[DIR_HIST_LENGTH] = {12, 12, 12, 12, 12};
+  int strHistory[STR_HIST_LENGTH] = {40, 40, 40, 40, 40};
+
+  // histories for greater accuracy
+  void updateStrHistory() {
+    for (int i = 0; i < STR_HIST_LENGTH; i++) {
+      if (i != STR_HIST_LENGTH-1) {
+        strHistory[i] = strHistory[i+1];
+      }
+      else {
+        strHistory[i] = strength;
+      }
+    }
+  };
+
+  void updateDirHistory() {
+    for (int i = 0; i < DIR_HIST_LENGTH; i++) {
+      if (i != DIR_HIST_LENGTH-1) {
+        dirHistory[i] = dirHistory[i+1];
+      }
+      else {
+        dirHistory[i] = direction;
+      }
+    }
+  };
+
+  // finds the most occuring IR reading from a history of directions
+  int mostCommonDirection() {
+    int directionCount[13] = {0};
+    
+    for (int i = 0; i < DIR_HIST_LENGTH; i++) {
+      directionCount[dirHistory[i]]++;
+    }
+    
+    int count = directionCount[dirHistory[DIR_HIST_LENGTH-1]];
+    int direction = dirHistory[DIR_HIST_LENGTH-1];
+    
+    for (int i = 0; i < 13; i++) {
+      if (directionCount[i] > count) {
+        count = directionCount[i];
+        direction = i;
+      }
+    }
+    return direction;
+  };
+
+public:
+  // reading data from ir sensor
+  void readData() {
+    Wire.requestFrom(0x08, 2);
+    if (Wire.available()) {
+      direction = Wire.read();
+      strength = Wire.read();
+    }
+
+    // updating histories
+    updateDirHistory();
+    updateStrHistory();
+  };
+
+  int getRawDirection() {
+    return direction;
+  };
+
+  int getRawStrength() {
+    return strength;
+  };
+
+  // filtering direction using history and most common values
+  int getFilteredDirection() {
+    return mostCommonDirection();
+  };
+
+  // fitering strength by finding average
+  int getFilteredStrength() {
+    int totalStrength = 0;
+    for (int i = 0; i < STR_HIST_LENGTH; i++) {
+      totalStrength += strHistory[i];
+    }
+    return totalStrength / (STR_HIST_LENGTH + 0.0);
+  };
+};
---
+++ code-2024/nationals/Final-Nationals-20-10/y_robot.ino
@@ -0,0 +1,203 @@
+// main robot class
+class Robot {
+public:
+  // defining objects
+  MotorController mc = MotorController(false);
+  Adafruit_BNO055 bno = Adafruit_BNO055(55);
+  IRSensor ir;
+
+  PositionSystem ps;
+
+  // defining variables
+  float tilt = 0;
+  float offset = 0;
+  int direction = 0, strength = 0;
+
+  float x = 0, y = 0;
+
+  int lineValue;
+  float previousY;
+
+private:
+  /* :::::::: FUNCTIONS :::::::: */
+  void getSensorData() {
+    // READ IR
+    ir.readData();
+    direction = ir.getFilteredDirection();
+    strength = ir.getFilteredStrength();
+
+    // READ IMU
+    sensors_event_t event;
+    bno.getEvent(&event);
+    tilt = event.orientation.x;
+
+    // convert to unit circle tilt
+    tilt = 360 - tilt;
+
+    // get values from line sensor
+    if (Serial6.available()) {
+      lineValue = Serial6.read() - 1;
+    }
+  };
+  
+  void getBallXY(float& ballX, float& ballY) {
+    ballX = cos(irAngles[direction]);
+    ballY = sin(irAngles[direction]);
+  };
+
+public:
+  void update() {
+    // get sensor data using sensor classes
+    getSensorData();
+    Serial.println("Read sensors"); // code crashes without line between for some reason
+    ps.update(tilt*PI/180);
+  };
+
+  // function for robot to position itself behind the ball
+  void getBehindBall() {
+    if (direction == 0) {
+      x = 0, y = 0;
+      return;
+    }
+
+    if (strength < 34) {
+      getBallXY(x, y);
+      return;
+    }
+
+    if (direction == 12) {
+      x = 0, y = 1;
+      return;
+    }
+
+    if (direction == 6) {
+      x = 1, y = 0;
+      return;
+    }
+
+    float movementAngle;
+
+    float ballX = 0, ballY = 0;
+    getBallXY(ballX, ballY);
+
+    if (ballX < 0) {
+      movementAngle = irAngles[direction] + PI/2;
+    }
+    else {
+      movementAngle = irAngles[direction] - PI/2;
+    }
+
+    x = cos(movementAngle);
+    y = sin(movementAngle);
+  };
+
+  // adjusting speed according to situation
+  void adjustSpeed() {
+    if (lineValue != 0) {
+      mc.setSpeed(LINE_SPEED);
+      return;
+    }
+
+    if (irAngles[direction] > PI && irAngles[direction] < 2*PI) {
+      if (strength > 30) {
+        mc.setSpeed(BALL_BEHIND_CLOSE_SPEED);
+      }
+      else {
+        mc.setSpeed(BALL_BEHIND_SPEED);
+      }
+    }
+    else {
+      mc.setSpeed(100);
+    }
+  };
+
+  // tilt towards goal
+  void getOffset() {
+    if (lineValue != 0 || !ps.reliable) {
+      offset = 0;
+      return;
+    }
+
+    if (ps.x < FIELD_WIDTH/2 - 20) {
+      offset = 25;
+    }
+    else if (ps.x > FIELD_WIDTH/2 + 20) {
+      offset = -25;
+    }
+    else {
+      offset = 0;
+    }
+
+    // if (ps.x < 50) {
+    //   offset = -30;
+    // }
+    // else if (FIELD_WIDTH - ps.x < 50) {
+    //   offset = 0; // 30
+    // }
+    // else if (ps.x < (FIELD_WIDTH/2 - 20)) {
+    //   offset = -20;
+    // }
+    // else if (ps.x > (FIELD_WIDTH/2 + 20)) {
+    //   offset = 0; // 20
+    // }
+    // else {
+    //   offset = 0;
+    // }
+  };
+
+  // gets the goal x,y coordinants using ultrasonics
+  void getGoalXY(float &x, float &y) {
+    x = -(ps.x - GOAL_POS_X);
+    y = ps.y - GOAL_POS_Y;
+  };
+
+  void targetGoal() {
+    x = 0, y = 1;
+    // if (!ps.reliable || direction != 12) {
+    //   return;
+    // }
+    // getGoalXY(x, y);
+  };
+
+  // keep a record of previous Y heading
+  void logY() {
+    if (lineValue == 0) {
+      previousY = y;
+    }
+  };
+
+  // stays within the lines (lineValue 1 is front, 2 is left, 3 is right, 4 is back)
+  void stopAtLine() {
+
+    if (lineValue == 1 || lineValue == 4) {
+      if(previousY < 0) x = 0, y = 1;
+      else x = 0, y = -1;
+    }
+
+    else if (lineValue == 2) { // && (x < 0)
+      if (ps.x < 40) {
+        x = 1, y = 0;
+      }
+      else {
+        // set speed to 100 as last resort to get out quick
+        mc.setSpeed(100);
+        x = -1, y = 0;
+      }
+    }
+    else if (lineValue == 3) { //&& (x > 0)
+      if (ps.x < 40) {
+        // set speed to 100 as last resort to get out quick
+        x = 1, y = 0;
+      }
+      else {
+        mc.setSpeed(100);
+        x = -1, y = 0;
+      }
+    }
+  };
+
+  void run() {
+    // run motors using motor controller
+    mc.runMotors(x, y, tilt, offset);
+  };
+};
